4ce3cb63103ed533f13977c826f0d46430c0b4e6
stfRangga <stefanusrangga04@gmail.com>
2024-06-21 00:33:38 +0700

    prod setting

--- src/server/server.js
+++ src/server/server.js
@@ -6,8 +6,8 @@ const { loadCsvData, loadIngredientsCsvData } = require("../service/loadFood")
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: "127.0.0.1",
+    port: process.env.PORT || 8080,
+    host: "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
